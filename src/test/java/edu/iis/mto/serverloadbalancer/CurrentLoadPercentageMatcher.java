4f777a218da4a1d08085ce7f7461d4f8189ebf0b
Dominik <rakdominikrak@gmail.com>
2019-05-10 21:11:51 +0200

    refactorization, deletion of magic number, addVm method

--- src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -7,6 +7,7 @@ import org.hamcrest.TypeSafeMatcher;
 @SuppressWarnings("deprecation")
 public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
 
+    private static final double EPSILON = 0.01d;
     private double expectedLoadPercentage;
 
     public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
@@ -31,7 +32,7 @@ public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
     }
 
     private boolean doublesAreEqual(double d1, double d2) {
-        return d1 == d2 || Math.abs(d1 - d2) < 0.01d;
+        return d1 == d2 || Math.abs(d1 - d2) < EPSILON;
     }
 
     public static Matcher hasCurrentLoadOf(double expectedLoadPercentage) {
